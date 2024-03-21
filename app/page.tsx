97eaf27e2a0138b90855882cbe09a5dd495b9c73
2dowon <dwon424@gmail.com>
2024-03-21 21:11:29 +0900

    feat: layout of todolist

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hi</h1>
-      <Button>Click me</Button>
+    <div className="flex justify-center items-center h-screen bg-gray-50">
+      <div className="relative rounded-lg bg-white p-7 shadow-md h-[80vh] w-[500px]">
+        <h1 className="pb-[1rem] font-extrabold text-3xl">TO DO LIST</h1>
+
+        <div className="flex gap-[1rem]">
+          <Input placeholder="typing todo" />
+          <Button>Add</Button>
+        </div>
+      </div>
     </div>
   );
 }
