473c4b1d74d419d0173b5fbb1e4bf86e273f9205
2dowon <dwon424@gmail.com>
2024-03-21 21:18:31 +0900

    chore: update tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-50">
-      <div className="relative rounded-lg bg-white p-7 shadow-md h-[80vh] w-[500px]">
+      <div className="relative rounded-lg bg-white p-7 shadow-md sm:w-[500px] sm:h-[80vh] h-screen w-screen">
         <h1 className="pb-[1rem] font-extrabold text-3xl">TO DO LIST</h1>
 
         <div className="flex gap-[1rem]">
